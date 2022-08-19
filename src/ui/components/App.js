0497756c2aa5c63bf0667c23b8ef936a8770368f
nadpalu <nadia_lucadei@outlook.com>
2022-08-19 07:34:47 -0300

    Desafio: Menu E-Commerce

---
+++ src/ui/components/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header.js";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Header/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ui/components/Header.js
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar.js";
+
+function Header () {
+    return (
+        <div className="header">
+            <img className="imagenHeader" src="../../../assets/img/logo.png"></img>
+            <NavBar/>
+        </div>
+    )
+}
+	 
+export default Header;
